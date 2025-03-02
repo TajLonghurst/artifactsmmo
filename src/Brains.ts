3183affaaa54d7552e33aa470d1a33a4d78540d2
Taj Longhurst <78848899+TajLonghurst@users.noreply.github.com>
2025-03-02 19:45:53 +1300

    Update Brains.ts

--- src/Brains.ts
+++ src/Brains.ts
@@ -4,7 +4,7 @@ import FightChickens from "./commands/FightChickens.ts";
 import MineCopperOre from "./commands/MineCopperOre.ts";
 
 const Brains = async () => {
-  // MineCopperOre(env.CHARACTER);
+  MineCopperOre(env.CHARACTER);
   FightChickens(env.CHARACTER_ONE);
 };
 
